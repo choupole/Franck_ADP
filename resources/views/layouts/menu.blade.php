b4df1f16fb26435d4d2f32abd9498a68dc16eed1
Franck kapuya <franckkapuya13@gmail.com>
2023-07-12 16:08:41 +0100

    Initialisation du projet

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -5,90 +5,87 @@
         <p>Home</p>
     </a>
 </li>
+
+<li class="nav-item">
+    <a href="{{ route('projects.index') }}" class="nav-link {{ Request::is('projects') ? 'active' : '' }}">
+
+        <i class="nav-icon fas fa-folder"></i>
+
+        <p>Projects</p>
+    </a>
+</li>
+
+
+@access('read','ComplexityItem')
 <li class="nav-item">
     <a href="#" class="nav-link">
-      <i class="nav-icon fas fa-tachometer-alt"></i>
-      <p>
-        Dashboard
-        <i class="right fas fa-angle-left"></i>
-      </p>
+        <i class="nav-icon fas  fa-cogs"></i>
+        <p>
+            Configurations
+            <i class="right fas fa-angle-left"></i>
+        </p>
     </a>
     <ul class="nav nav-treeview">
-      <li class="nav-item">
-        <a href="../index.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
-          <p>Dashboard v1</p>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="../index2.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
-          <p>Dashboard v2</p>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="../index3.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
-          <p>Dashboard v3</p>
-        </a>
-      </li>
+        <li class="nav-item">
+            <a href="{{ route('messagesMails.index') }}"
+                class="nav-link {{ Request::is('messagesMails') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-envelope"></i>
+                <p>Mails</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('users.index') }}" class="nav-link {{ Request::is('users') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-user"></i>
+                <p>Listes des Utilisateurs</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('jalons.index') }}" class="nav-link {{ Request::is('jalons') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-history"></i>
+                <p>Jalon</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('directions.index') }}"
+                class="nav-link {{ Request::is('directions') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-address-book"></i>
+                <p>Directions</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('optionsttm.index') }}"
+                class="nav-link {{ Request::is('optionsttm') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-columns"></i>
+                <p>Option Ttm</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('roles.index') }}" class="nav-link {{ Request::is('roles') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-columns"></i>
+                <p>Roles</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('writelist.index') }}" class="nav-link {{ Request::is('writes') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-history"></i>
+                <p>Write list</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('projectType.index') }}"
+                class="nav-link {{ Request::is('projectType') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-square"></i>
+                <p>Types de projet</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('ComplexityItem.index') }}"
+                class="nav-link {{ Request::is('ComplexityItem') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-square"></i>
+                <p>Points de complexités</p>
+            </a>
+        </li>
     </ul>
 </li>
-<li class="nav-item">
-    <a href="#" class="nav-link">
-      <i class="nav-icon fas fa-tachometer-alt"></i>
-      <p>
-        Dashboard
-        <i class="right fas fa-angle-left"></i>
-      </p>
-    </a>
-    <ul class="nav nav-treeview">
-      <li class="nav-item">
-        <a href="../index.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
-          <p>Dashboard v1</p>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="../index2.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
-          <p>Dashboard v2</p>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="../index3.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
-          <p>Dashboard v3</p>
-        </a>
-      </li>
-    </ul>
-</li>
-<li class="nav-item">
-    <a href="#" class="nav-link">
-      <i class="nav-icon fas fa-tachometer-alt"></i>
-      <p>
-        Dashboard
-        <i class="right fas fa-angle-left"></i>
-      </p>
-    </a>
-    <ul class="nav nav-treeview">
-      <li class="nav-item">
-        <a href="../index.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
-          <p>Dashboard v1</p>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="../index2.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
-          <p>Dashboard v2</p>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="../index3.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
-          <p>Dashboard v3</p>
-        </a>
-      </li>
-    </ul>
-</li>
+@endaccess
+
